3667bf546d9f8d6ebd0c4867ee5c52a7473199fe
Subhashini Vijayraj <svijayraj1@sheffield.ac.uk>
2024-05-02 20:37:15 +0100

    added navigation to details page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* Plant Details */
+router.get('/plantdetails', function(req, res, next) {
   res.render('plantdetails', { title: 'Express' });
 });
 
