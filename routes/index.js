226167cf9e3661aefdc6dae0a618dc7b7d8a0e19
Siddhant Sahni <ssahni1@sheffield.ac.uk>
2024-05-02 20:22:31 +0100

    Merged main into branch and added route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('plantdetails', { title: 'Express' });
 });
 
+router.get('/main', function(req, res, next) {
+  res.render('main', { title: 'Express' });
+});
+
 module.exports = router;
