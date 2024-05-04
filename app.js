5540c3640087bdc89e3eecf1d275b968d35e1822
rani kumari <rkumari1@sheffield.ac.uk>
2024-05-04 11:45:52 +0100

    started

--- app.js
+++ app.js
@@ -4,45 +4,69 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var { initDatabase } = require('./indexdb/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Initialize IndexedDB
+initDatabase()
+    .then(() => {
+      console.log('IndexedDB initialized successfully.');
+      // Continue with server setup
+      startServer();
+    })
+    .catch(error => {
+      console.error('Failed to initialize IndexedDB:', error);
+    });
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/public/images/uploads', express.static(path.join(__dirname, '/public/images/uploads')));
+function startServer()
+{
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use('/public/images/uploads', express.static(path.join(__dirname, '/public/images/uploads')));
 
-app.get('/newPlant', (req, res) => {
-  res.render('newPlant');
-});
+  app.use('/', indexRouter);
+  // app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  app.get('/newPlant', (req, res) => {
+    res.render('newPlant');
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  // Start the server
+    var port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+}
 
 module.exports = app;
+
+
+
